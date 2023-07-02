911b71feec65338a9188ed0389acefeacfbe6d7e
Erik Hyrkas <erik.hyrkas@gmail.com>
2023-07-02 13:38:14 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # vector_db
-This is a simple local vector database that integrates with the huggingface model gtr-t5-large.
+This is a simple, in-memory local vector database that integrates with the huggingface model gtr-t5-large.
+
+One might use this to quickly search for the most relevant document.
+
+Because it is in-memory, it isn't suitable to large repositories of documents, but there are still many useful usecases for this.
 
--- main.py
+++ main.py
@@ -5,15 +5,9 @@ import pickle
 import gzip
 from sentence_transformers import SentenceTransformer
 
-##########################################
-# Configuration
-embedding_model = "sentence-transformers/gtr-t5-large"
-db_file_name = "documents.bin"
-##########################################
-
 
 class LocalVectorDb:
-    def __init__(self, documents=None):
+    def __init__(self, documents=None, embedding_model="sentence-transformers/gtr-t5-large"):
         self.sentence_transformer = SentenceTransformer(embedding_model)
         self.embeddings = None
         self.documents = documents
@@ -42,36 +36,32 @@ class LocalVectorDb:
         self.embeddings = data["embeddings"].astype(np.float32)
         self.documents = data["documents"]
 
-    def best(self, query_vector):
-        similarity = self.cosine_similarity(query_vector)
-        top_index = np.argsort(similarity, axis=0)[-1:][::-1]
-        return top_index.flatten()[0]
+    def best_index(self, query_vector):
+        return np.argsort(self.cosine_similarity(query_vector), axis=0)[-1:][::-1][0]
 
     def query(self, query):
         query_vector = self.embed([query])[0]
-        best_result = self.best(query_vector)
-        return self.documents[best_result]
+        return self.documents[self.best_index(query_vector)]
 
 
-def read_adoc_files(directory):
+def read_text_files(directory):
     print("Reading text documents", end='')
     file_contents = []
 
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith(".adoc"):
+            if file.endswith(".adoc") or file.endswith(".txt"):
                 file_path = os.path.join(root, file)
                 with open(file_path, "r", encoding="utf-8") as f:
                     print(".", end='')
-                    next_string = f.read()
-                    file_contents.append(next_string)
+                    file_contents.append(f.read())
 
     print("")
     print("Done.")
     return file_contents
 
 
-def load_or_build_vector_db(documents_folder):
+def load_or_build_vector_db(documents_folder, db_file_name="documents.bin"):
     if os.path.exists(db_file_name):
         print("Loading existing vector database...")
         db = LocalVectorDb()
@@ -80,7 +70,7 @@ def load_or_build_vector_db(documents_folder):
         return db
 
     print("Building vector database for first run.")
-    all_contents = read_adoc_files(documents_folder)
+    all_contents = read_text_files(documents_folder)
     db = LocalVectorDb(all_contents)
     db.save(db_file_name)
     print("Created.")
